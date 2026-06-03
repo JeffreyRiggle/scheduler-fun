ec3cf8d4fbb0454485322f9215a1a460602b6427
Jeffrey Riggle <jeffreyriggle@gmail.com>
2026-06-03 14:07:44 -0400

    fix(refactor): Clean up logic a bit

--- kqueue-example/main.c
+++ kqueue-example/main.c
@@ -17,16 +17,67 @@ const char *get_response =
   "\r\n"
   "<html><body><form action=\"file\" method=\"post\"><label for=\"fscript\">Create a script</label><input type=\"text\" name=\"fscript\" required /><input type=\"submit\" value=\"send\"/></form></body></html>";
 
-void set_nonblocking(int fd) {
+int set_nonblocking(int fd) {
     int flags = fcntl(fd, F_GETFL, 0);
     if (flags == -1) {
-        perror("fcntl F_GETFL");
-        exit(EXIT_FAILURE);
+      return flags;
     }
-    if (fcntl(fd, F_SETFL, flags | O_NONBLOCK) == -1) {
-        perror("fcntl F_SETFL");
-        exit(EXIT_FAILURE);
+
+    return fcntl(fd, F_SETFL, flags | O_NONBLOCK); 
+}
+
+void process_event(struct kevent evt, int server_fd, struct kevent* change_event, int kqueue_fd) {
+  int target_fd = evt.ident;
+
+  if (evt.flags & EV_EOF) {
+    printf("Client has disconnected closing fd %d\n", target_fd);
+    close(target_fd);
+    return; 
+  }
+
+  if (target_fd == server_fd) {
+    struct sockaddr_in client_addr;
+    socklen_t client_len = sizeof(client_addr);
+    int client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &client_len);
+
+    if (client_fd == -1) {
+      fprintf(stderr, "Failed to accept connection!\n");
+      return; 
     }
+
+    int nonblocking_res = set_nonblocking(client_fd);
+    if (nonblocking_res == -1) {
+      fprintf(stderr, "Failed to set client connection to nonblocking terminiating connection\n");
+      close(client_fd);
+      return;
+    }
+
+    EV_SET(change_event, client_fd, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, NULL);
+    if (kevent(kqueue_fd, change_event, 1, NULL, 0, NULL) == -1) {
+      fprintf(stderr, "Failed to register client to a new socket\n");
+      close(client_fd);
+    }
+    return;
+  }
+
+  if (evt.filter == EVFILT_READ) {
+    char buffer[1024];
+    memset(buffer, 0, 1024);
+    ssize_t bytes_read = read(target_fd, buffer, sizeof(buffer) - 1);
+    if (bytes_read > 0) {
+      // TODO actually handle all the same crap as before
+      printf("Got data %s\n", buffer);
+      write(target_fd, get_response, bytes_read);
+      return;
+    }
+
+    if (bytes_read == 0) {
+      printf("Unable to read data killing connection or something\n");
+      close(target_fd);
+      return;
+    }
+    printf("Failed to read data\n");
+  }
 }
 
 // TODO make this easier to read
@@ -64,7 +115,11 @@ int main() {
     return -1;
   }
 
-  set_nonblocking(server_fd);
+  int nonblocking_res = set_nonblocking(server_fd);
+  if (nonblocking_res == -1) {
+    fprintf(stderr, "Failed to set to nonblocking mode. Closing server socket!\n");
+    return -1;
+  }
   printf("Server is listenting on port 3000...\n");
 
   kqueue_fd = kqueue();
@@ -82,7 +137,6 @@ int main() {
   }
 
   while (1) {
-    // DO the work
     int event_count = kevent(kqueue_fd, NULL, 0, event_list, 32, NULL);
     if (event_count == -1) {
       fprintf(stderr, "Failed to wait on kqueue\n");
@@ -90,47 +144,8 @@ int main() {
     }
 
     for (int i = 0; i < event_count; i++) {
-      int target_fd = event_list[i].ident;
-
-      if (event_list[i].flags & EV_EOF) {
-        printf("Client has disconnected closing fd %d\n", target_fd);
-        close(target_fd);
-        continue;
-      }
-
-      if (target_fd == server_fd) {
-        struct sockaddr_in client_addr;
-        socklen_t client_len = sizeof(client_addr);
-        int client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &client_len);
-
-        if (client_fd == -1) {
-          fprintf(stderr, "Failed to accept connection!\n");
-          continue;
-        }
-
-        set_nonblocking(client_fd);
-
-        EV_SET(&change_event, client_fd, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, NULL);
-        if (kevent(kqueue_fd, &change_event, 1, NULL, 0, NULL) == -1) {
-          fprintf(stderr, "Failed to register client to a new socket\n");
-          close(client_fd);
-        }
-      }
-      else if (event_list[i].filter == EVFILT_READ) {
-        char buffer[1024];
-        memset(buffer, 0, 1024);
-        ssize_t bytes_read = read(target_fd, buffer, sizeof(buffer) - 1);
-        if (bytes_read > 0) {
-          // TODO actually handle all the same crap as before
-          printf("Got data %s\n", buffer);
-          write(target_fd, get_response, bytes_read);
-        } else if (bytes_read == 0) {
-          printf("Unable to read data killing connection or something\n");
-          close(target_fd);
-        } else {
-          printf("Failed to read data\n");
-        }
-      }
+      struct kevent target_event = event_list[i];
+      process_event(target_event, server_fd, &change_event, kqueue_fd);
     }
   }
 
